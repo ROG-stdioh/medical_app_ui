fe8a3083a19042aed2821a19573792372971d56f
ROG-stdioh <90563301+ROG-stdioh@users.noreply.github.com>
2022-04-18 02:14:09 +0530

    card - "How do you feel today"

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -27,6 +27,7 @@ class _HomePageState extends State<HomePage> {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+                      SizedBox(height: 16),
                       Text(
                         "Hello, ",
                         style: TextStyle(
@@ -56,9 +57,72 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               ),
-            )
+            ),
 
-            // card -> How do you feel about it?
+            SizedBox(height: 25),
+
+            // card -> How do you feel?
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.pink[100],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    children: [
+                      // animation or cute picture
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.deepPurple[200],
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+
+                      // how do you feel - get started button
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'How do you feel?',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(height: 12),
+                            Text(
+                              'Fill out your medical card right now',
+                              style: TextStyle(
+                                fontSize: 14,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Container(
+                              padding: EdgeInsets.all(12),
+                              decoration: BoxDecoration(
+                                color: Colors.deepPurple[300],
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Get Started',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  )),
+            ),
+
+            SizedBox(height: 25),
 
             // search bar
 
